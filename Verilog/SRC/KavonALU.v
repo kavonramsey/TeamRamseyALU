c1b0c76d53927eba5e9e4456265a5d72515f6831
kavonramsey <44102962+kavonramsey@users.noreply.github.com>
2020-11-17 10:11:33 -0600

    Add files via upload
    This should now have the output fully working

--- Verilog/SRC/KavonALU.v
+++ Verilog/SRC/KavonALU.v
@@ -14,6 +14,32 @@ module Mux16(a15, a14, a13, a12, a11, a10, a9, a8, a7, a6, a5, a4, a3, a2, a1, a
    input [k-1:0] a15, a14, a13, a12, a11, a10, a9, a8, a7, a6, a5, a4, a3, a2, a1, a0;
    input [3:0] s; // 4-bit opcode 
    output [k-1:0] b;
+   reg [k-1:0] b;
+   always @ (*)
+     begin
+	case(s)
+	  4'b0000:b=a15;
+	  4'b0001:b=a14;
+	  4'b0010:b=a13;
+	  4'b0011:b=a12;
+	  4'b0100:b=a11;
+	  4'b0101:b=a10;
+	  4'b0110:b=a9;
+	  4'b0111:b=a8;
+	  4'b1000:b=a7;
+	  4'b1001:b=a6;
+	  4'b1010:b=a5;
+	  4'b1011:b=a4;
+	  4'b1100:b=a3;
+	  4'b1101:b=a2;
+	  4'b1110:b=a1;
+	  4'b1111:b=a0;
+	  default:b=4'b0000;
+	endcase // case (s)
+     end // always @ (*)
+   
+  
+   /**
       assign b = ({k{s[15]}} & a15) |
 		 ({k{s[14]}} & a14) |
 		 ({k{s[13]}} & a13) |
@@ -30,6 +56,7 @@ module Mux16(a15, a14, a13, a12, a11, a10, a9, a8, a7, a6, a5, a4, a3, a2, a1, a
 		 ({k{s[2]}} & a2) |
 		 ({k{s[1]}} & a1) |
 		 ({k{s[0]}} & a0) ;
+    **/
    endmodule // Mux16
 
 `define NOOP 4'b0000
@@ -87,7 +114,8 @@ module ALU(clk, clear, input1, input2, opcode, out);
 		  (opcode == `RESET) & ~clear,
 		  clear}, next);
    **/
-   //Mux16 choice(modeNOOP, add_val, sub_val, product[15:0], quotient, and_val, or_val, xor_val, not_val, muxPORT9, muxPORT10, muxPORT11, muxPORT12, muxPORT13, muxPORT14, modeRESET, opcode, next);
+   Mux16 choice(modeNOOP, add_val, sub_val, product[15:0], quotient, and_val, or_val, xor_val, not_val, muxPORT9, muxPORT10, muxPORT11, muxPORT12, muxPORT13, muxPORT14, modeRESET, opcode, next);
+   /**
    Mux16 choice(modeNOOP, add_val, sub_val, product[15:0], quotient, and_val, or_val, xor_val, not_val, muxPORT9, muxPORT10, muxPORT11, muxPORT12, muxPORT13, muxPORT14, modeRESET, 
 		{(opcode == `NOOP),
 		 (opcode == `ADD),
@@ -99,7 +127,7 @@ module ALU(clk, clear, input1, input2, opcode, out);
 		 (opcode == `XOR),
 		 (opcode == `NOT),
 		 (opcode == `RESET)}, next);
-
+**/
    assign out = next;
 
 endmodule // ALU
